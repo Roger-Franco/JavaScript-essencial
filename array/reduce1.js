466cc34a65fd910bf566fb07b0d72533db4f5db3
Roger-Franco <rogerfrancogalt@gmail.com>
2022-07-09 13:04:23 -0400

    Array: Reduce #03

--- array/reduce1.js
+++ array/reduce1.js
@@ -1,14 +1,25 @@
-const alunos = [
-  { nome: 'João', nota: 7.3, bolsista: false },
-  { nome: 'Maria', nota: 9.2, bolsista: true },
-  { nome: 'Pedro', nota: 9.8, bolsista: false },
-  { nome: 'Ana', nota: 8.7, bolsista: true }
-]
+// Array.prototype.reduce2 = function(callback) {
+//   let acumulador = this[0]
+//   for (let i = 1; i < this.length; i++) {
+//       acumulador = callback(acumulador, this[i], i, this)
+//   }
+//   return acumulador
+// }
 
-console.log(alunos.map(a => a.nota))
-const resultado = alunos.map(a => a.nota).reduce(function (acumulador, atual) {
-  console.log(acumulador, atual)
-  return acumulador + atual
-}, 0)
+// const soma = (total, valor) => total + valor
+// const nums = [1, 2, 3, 4, 5, 6]
+// console.log(nums.reduce2(soma))
 
-console.log(resultado)
+/* ----- */
+Array.prototype.reduce2 = function (callback, valorInicial) {
+  const indiceInicial = valorInicial ? 0 : 1
+  let acumulador = valorInicial || this[0]
+  for (let i = indiceInicial; i < this.length; i++) {
+    acumulador = callback(acumulador, this[i], i, this)
+  }
+  return acumulador
+}
+
+const soma = (total, valor) => total + valor
+const nums = [1, 2, 3, 4, 5, 6]
+console.log(nums.reduce2(soma, 21))
